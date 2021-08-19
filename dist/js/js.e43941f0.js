eb3bbe60edf077e4d63955c7c97e4d1b35abfe54
DuckenClaw <51297941+DuckenClaw@users.noreply.github.com>
2021-08-19 15:58:03 +0300

    finished

--- dist/js/js.e43941f0.js
+++ dist/js/js.e43941f0.js
@@ -11258,7 +11258,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62412" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52479" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
